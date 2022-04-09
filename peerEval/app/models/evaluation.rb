9441087ac9945fb8e80f3435867549f91192fadd
Suyash Talekar <talekar.2@osu.edu>
2022-04-09 19:18:27 -0400

    Implemented project-evaluation relationship

--- peerEval/app/models/evaluation.rb
+++ peerEval/app/models/evaluation.rb
@@ -1,7 +1,7 @@
 class Evaluation < ApplicationRecord
     validates :score, presence: true, inclusion: {in: (1..5), message: "%{value} is not a valid size"}
     validates :comments, presence: true, length: { maximum: 500, minimum: 1}
-
+    belongs_to :project
 
 end
 
--- peerEval/app/models/project.rb
+++ peerEval/app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
     validates :name, presence: true, length: { maximum: 50}, uniqueness: { case_sensitive: false}
+    has_many :evaluations
 end
