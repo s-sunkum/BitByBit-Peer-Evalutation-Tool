b672582813559c86d22edf39b9304fe0f049d7ed
s-sunkum <sunkum.2@osu.edu>
2022-04-10 15:36:43 -0400

    Added association between student and evaluation. Still some errors when testing

--- peerEval/db/schema.rb
+++ peerEval/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_001617) do
+ActiveRecord::Schema.define(version: 2022_04_10_192354) do
 
   create_table "evaluations", force: :cascade do |t|
     t.integer "score"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "comments"
-    t.integer "project_id"
+    t.integer "student_id"
   end
 
   create_table "projects", force: :cascade do |t|
