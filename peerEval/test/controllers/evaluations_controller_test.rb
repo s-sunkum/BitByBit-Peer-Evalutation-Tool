1bc743e3eed98aaffc58c524bd053bd694b1c1e7
Yusen95 <zhang.10095@buckeyemail.osu.edu>
2022-04-22 16:11:31 -0400

    Try to add controller tests

--- peerEval/test/controllers/evaluations_controller_test.rb
+++ peerEval/test/controllers/evaluations_controller_test.rb
@@ -5,4 +5,20 @@ class EvaluationsControllerTest < ActionDispatch::IntegrationTest
     get evaluations_new_url
     assert_response :success
   end
+
+  test "should get index" do
+    get evaluations_index_url
+    assert_response :success
+  end
+
+#   test "should get index" do
+#     get evaluations_url
+#     assert_response :success
+#   end
+
+test "should get show" do
+    get evaluations_show_url
+    assert_response :success
+  end
+  
 end
--- peerEval/test/controllers/teachers_controller_test.rb
+++ peerEval/test/controllers/teachers_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class TeachersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get teachers_new_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get teachers_new_url
+  #   assert_response :success
+  # end
 end
